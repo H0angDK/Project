6ceec59e181931c3e744c3e93e1e767345adeed3
Hoang <dinkonghoang510@gmail.com>
2023-06-30 21:04:46 +0300

    fixed bugs

--- task02.h
+++ task02.h
@@ -1,24 +1,31 @@
 #pragma once
 #include <iostream>
 using namespace std;
-int recursion(int a, int k, int n) {
-    if (n == 1) return a;
-    return recursion(a, k, n - 1) + k;
+int recursion(int a, int d, int n, int& count) {
+    ++count;
+    if (n == 1)
+        return a;
+    else
+        return recursion(a + d, d, n - 1, count);
 }
-int cycle(int a, int k, int n) {
-    for (int i = 1; i < n; i++)
-        a += k;
-    return a;
+
+int cycle(int a, int d, int n, int& count) {
+    int term = a;
+    for (int i = 2; i <= n; ++i) {
+        ++count;
+        term += d;
+    }
+    return term;
 }
-void task02()
-{
-    int a1, k, n;
+
+void task02() {
+    int a1, k, n, countRecursive = 0, countCycle = 0;
     cout << "First element\n";
     cin >> a1;
     cout << "difference\n";
     cin >> k;
     cout << "member\n";
     cin >> n;
-    cout << recursion(a1, k, n) << endl;
-    cout << cycle(a1, k, n) << endl;
+    cout << "recursion\n" << "result:\t" << recursion(a1, k, n, countRecursive) << "\nthe number of iterations is " << countRecursive << endl;
+    cout << "cycle\n" << "result:\t" << cycle(a1, k, n, countCycle) << "\nthe number of iterations is " << countCycle << endl;
 }
